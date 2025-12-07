ff350898acaeee6d6f1256ab3686ec3db61bdbe6
bautista-arch <bautistakearl@gmail.com>
2025-12-07 09:45:27 +0800

    Update AfterLoginPage.java

--- AfterLoginPage.java
+++ AfterLoginPage.java
@@ -1,7 +1,28 @@
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.AbstractBorder;
 
 public class AfterLoginPage extends JFrame {
+
+    static class RoundedBorder extends AbstractBorder {
+        private int radius;
+        private Color color;
+        private int thickness;
+
+        public RoundedBorder(int radius, Color color, int thickness) {
+            this.radius = radius;
+            this.color = color;
+            this.thickness = thickness;
+        }
+
+        @Override
+        public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setColor(color);
+            g2.setStroke(new BasicStroke(thickness));
+            g2.drawRoundRect(x + thickness / 2, y + thickness / 2, width - thickness, height - thickness, radius, radius);
+        }
+    }
 
     public AfterLoginPage() {
 
@@ -9,20 +30,14 @@ public class AfterLoginPage extends JFrame {
         setSize(1200, 700);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        setLayout(null);
-        getContentPane().setBackground(Color.WHITE);
 
-        addTopNav();
-        addBanner();
-        addMenu();
-        addCategoryPanel();
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(null);
+        mainPanel.setBackground(Color.WHITE);
+        mainPanel.setPreferredSize(new Dimension(1200, 800)); 
 
-        setVisible(true);
-    }
-
-    // ---------------- TOP NAVIGATION BAR ----------------
-    private void addTopNav() {
-        JPanel topNav = new JPanel(null);
+        JPanel topNav = new JPanel();
+        topNav.setLayout(null);
         topNav.setBackground(Color.WHITE);
         topNav.setBounds(0, 0, 1200, 70);
 
@@ -48,81 +63,124 @@ public class AfterLoginPage extends JFrame {
         userButton.setBorder(BorderFactory.createLineBorder(Color.GRAY, 2));
         topNav.add(userButton);
 
-        add(topNav);
-    }
+        mainPanel.add(topNav);
 
-    // ---------------- BANNER SECTION ----------------
-    private void addBanner() {
-        JPanel banner = new JPanel(null);
+        JPanel banner = new JPanel();
+        banner.setLayout(null);
         banner.setBackground(Color.BLACK);
-        banner.setBounds(50, 90, 1100, 220);
-        banner.setBorder(BorderFactory.createLineBorder(Color.BLACK, 4));
+        banner.setBounds(40, 100, 1100, 220);
+        banner.setBorder(new RoundedBorder(30, Color.BLACK, 4));
 
         JLabel bannerText = new JLabel("BANNER");
         bannerText.setForeground(Color.WHITE);
         bannerText.setFont(new Font("Arial", Font.BOLD, 36));
-        bannerText.setBounds(430, 80, 300, 50);
+        bannerText.setBounds(480, 80, 300, 50);
         banner.add(bannerText);
 
         JLabel dots = new JLabel("● ● ●");
         dots.setFont(new Font("Arial", Font.BOLD, 18));
         dots.setForeground(Color.WHITE);
-        dots.setBounds(520, 150, 80, 30);
+        dots.setBounds(540, 155, 80, 30);
         banner.add(dots);
 
-        add(banner);
-    }
+        mainPanel.add(banner);
 
-    // ---------------- MENU BAR ----------------
-    private void addMenu() {
-        JPanel menuPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 40, 5));
+        JPanel menuPanel = new JPanel();
+        menuPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 40, 5));
         menuPanel.setBackground(Color.WHITE);
-        menuPanel.setBounds(0, 320, 1200, 40);
+        menuPanel.setBounds(0, 320, 1200, 50);
 
         String[] menuItems = {"Home", "Departments", "Recently Added", "New Arrivals"};
 
         for (String item : menuItems) {
-            JLabel menuLabel = new JLabel(item);
-            menuLabel.setFont(new Font("Arial", Font.BOLD, 14));
+            JLabel menuLabel = new JLabel(item, SwingConstants.CENTER);
+            menuLabel.setFont(new Font("Arial", Font.BOLD, 16));
             menuLabel.setForeground(new Color(20, 40, 70));
+            menuLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+            menuLabel.setPreferredSize(new Dimension(150, 30));
+            menuLabel.setHorizontalAlignment(SwingConstants.CENTER);
             menuPanel.add(menuLabel);
+
+            if (item.equals("Departments")) {
+                JPopupMenu deptMenu = new JPopupMenu();
+                String[] departments = {"BASIC ED", "CBA", "CHM", "CCS", "COF"};
+
+                for (String dept : departments) {
+                    JMenuItem menuItem = new JMenuItem(dept);
+                    menuItem.addActionListener(e -> {
+                        JOptionPane.showMessageDialog(this, "You selected: " + dept);
+                    });
+                    deptMenu.add(menuItem);
+                }
+
+                menuLabel.addMouseListener(new java.awt.event.MouseAdapter() {
+                    @Override
+                    public void mouseClicked(java.awt.event.MouseEvent e) {
+                        deptMenu.show(menuLabel, 0, menuLabel.getHeight());
+                    }
+                });
+            } else {
+                menuLabel.addMouseListener(new java.awt.event.MouseAdapter() {
+                    @Override
+                    public void mouseClicked(java.awt.event.MouseEvent e) {
+                        JOptionPane.showMessageDialog(null, item + " clicked!");
+                    }
+                });
+            }
         }
 
-        add(menuPanel);
-    }
+        mainPanel.add(menuPanel);
+            JPanel categoryPanel = new JPanel();
+            categoryPanel.setLayout(null);
+            categoryPanel.setBackground(new Color(233, 236, 239));
+            categoryPanel.setBounds(50, 370, 1100, 320);
+            categoryPanel.setBorder(new RoundedBorder(30, Color.GRAY, 4));
 
-    // ---------------- CATEGORY PANEL ----------------
-    private void addCategoryPanel() {
-        JPanel category = new JPanel(null);
-        category.setBackground(new Color(233, 236, 239));
-        category.setBounds(50, 370, 1100, 320);
-        category.setBorder(BorderFactory.createLineBorder(Color.GRAY, 3));
+            JLabel ccsTitle = new JLabel("CCS", SwingConstants.CENTER);
+            ccsTitle.setFont(new Font("Arial", Font.BOLD, 28));
+            ccsTitle.setForeground(new Color(20, 40, 100));
+            ccsTitle.setBounds(0, 10, 1100, 40);
+            categoryPanel.add(ccsTitle);
 
-        JLabel ccsTitle = new JLabel("CCS", SwingConstants.CENTER);
-        ccsTitle.setFont(new Font("Arial", Font.BOLD, 28));
-        ccsTitle.setForeground(new Color(20, 40, 100));
-        ccsTitle.setBounds(0, 10, 1100, 40);
-        category.add(ccsTitle);
+            JLabel subtitle = new JLabel("COLLEGE OF COMPUTER STUDIES", SwingConstants.CENTER);
+            subtitle.setFont(new Font("Arial", Font.PLAIN, 18));
+            subtitle.setBounds(0, 45, 1100, 30);
+            categoryPanel.add(subtitle);
 
-        JLabel subtitle = new JLabel("COLLEGE OF COMPUTER STUDIES", SwingConstants.CENTER);
-        subtitle.setFont(new Font("Arial", Font.PLAIN, 18));
-        subtitle.setBounds(0, 45, 1100, 30);
-        category.add(subtitle);
 
-        // 4 rectangular blocks
-        for (int i = 0; i < 4; i++) {
+            String[] images = {
+            "c:\\Users\\ASUS\\Documents\\CCS_Image1.png",
+            "c:\\Users\\ASUS\\Documents\\CCS_Image2.png",
+            "c:\\Users\\ASUS\\Documents\\CCS_Image3.png",
+            "c:\\Users\\ASUS\\Documents\\CCS_Image4.png"
+            };
+
+            for (int i = 0; i < 4; i++) {
             JPanel block = new JPanel();
             block.setBackground(Color.WHITE);
-            block.setBorder(BorderFactory.createLineBorder(Color.GRAY, 2));
             block.setBounds(70 + (i * 250), 100, 200, 180);
-            category.add(block);
+            block.setBorder(new RoundedBorder(20, Color.GRAY, 3));
+            block.setLayout(new BorderLayout());
+
+            ImageIcon icon = new ImageIcon(images[i]);
+            Image img = icon.getImage().getScaledInstance(200, 180, Image.SCALE_SMOOTH);
+            JLabel imgLabel = new JLabel(new ImageIcon(img));
+            block.add(imgLabel);
+
+            categoryPanel.add(block);
         }
 
-        add(category);
+        mainPanel.add(categoryPanel);
+
+        JScrollPane scrollPane = new JScrollPane(mainPanel);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        setContentPane(scrollPane);
+
+        setVisible(true);
     }
 
-    // ---------------- MAIN ----------------
     public static void main(String[] args) {
-        new AfterLoginPage();
+        SwingUtilities.invokeLater(() -> new AfterLoginPage());
     }
 }
