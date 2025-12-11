cd885a7ff85e59ad8b0b3093eaac30140492a1fb
bautista-arch <bautistakearl@gmail.com>
2025-12-11 08:46:59 +0800

    Add CartItem class with properties and constructor

---
+++ CartItem.java
@@ -0,0 +1,11 @@
+public class CartItem {
+    public String title;
+    public String price;
+    public String imagePath;
+
+    public CartItem(String title, String price, String imagePath) {
+        this.title = title;
+        this.price = price;
+        this.imagePath = imagePath;
+    }
+}
