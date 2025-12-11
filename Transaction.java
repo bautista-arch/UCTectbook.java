2ae85cfe8c4f788ab8ed4afa13aab1bb9c7f5134
bautista-arch <bautistakearl@gmail.com>
2025-12-11 09:52:03 +0800

    Create Transaction.java

---
+++ Transaction.java
@@ -0,0 +1,11 @@
+public class Transaction {
+    public String title;
+    public String price;
+    public String date;
+
+    public Transaction(String title, String price, String date) {
+        this.title = title;
+        this.price = price;
+        this.date = date;
+    }
+}
