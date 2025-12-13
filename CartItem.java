1a2597342863c8999b721b5b5279e04a35d87bd3
bautista-arch <bautistakearl@gmail.com>
2025-12-13 08:18:52 +0800

    Update CartItem.java

--- CartItem.java
+++ CartItem.java
@@ -2,10 +2,19 @@ public class CartItem {
     public String title;
     public String price;
     public String imagePath;
+    public String description;
 
     public CartItem(String title, String price, String imagePath) {
         this.title = title;
         this.price = price;
         this.imagePath = imagePath;
+        this.description = "";
+    }
+
+    public CartItem(String title, String price, String imagePath, String description) {
+        this.title = title;
+        this.price = price;
+        this.imagePath = imagePath;
+        this.description = description;
     }
 }
